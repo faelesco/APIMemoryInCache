6633bde83e461bb08e39834b437d803332889716
Raphael Lelis <raphalelis@outlookc.com>
2022-10-28 20:29:34 -0300

    Adicionar arquivos de projeto.

---
+++ APIMemoryInCache-Countries/Controllers/CountriesController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using APIMemoryInCache_Countries.Model;
+using Microsoft.Extensions.Caching.Memory;
+using System.Text.Json;
+
+
+namespace APIMemoryInCache_Countries.Controllers
+{
+    [Route("api/[Controller]")]
+    public class CountriesController : ControllerBase
+    {
+        private readonly IMemoryCache _memoryCache;
+        private const string COUNTRIES_KEY = "Countries";
+        public CountriesController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            const string restCountriesUrl = "https://viacep.com.br/ws/01001000/json/";
+
+            if (_memoryCache.TryGetValue(COUNTRIES_KEY, out List<Country> countries))
+            {
+                return Ok(countries);
+            }
+
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.GetAsync(restCountriesUrl);
+                
+                var responseData = await response.Content.ReadAsStringAsync();
+
+                countries = JsonSerializer.Deserialize<List<Country>>(responseData, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+
+                var memoryCacheEntryOption = new MemoryCacheEntryOptions
+                {
+                    AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(3600),
+                    SlidingExpiration = TimeSpan.FromSeconds(1200)
+                };
+
+                _memoryCache.Set(COUNTRIES_KEY, countries, memoryCacheEntryOption);
+
+                return Ok(countries);
+            }
+        }
+    }
+}
---
+++ APIMemoryInCache-Countries/Model/Country.cs
@@ -0,0 +1,12 @@
+﻿namespace APIMemoryInCache_Countries.Model
+{
+    public class Country
+    {
+        public string? Logradouro { get; set; }
+        public string? Complemento { get; set; }
+        public string? Bairro { get; set; }
+        public string? Localidade { get; set; }
+        public string? Uf { get; set; }
+    }
+
+}
